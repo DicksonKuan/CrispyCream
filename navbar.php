4a277f5ea903eddfcc07da132e2b2cebcf07b851
Dickson Kuan <s10192803@connect.np.edu.sg>
2022-01-06 11:40:15 +0800

    Added framework['s skeleton

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<?php 
+//Display guest welcome message, Login and Registration links
+//when shopper has yet to login,
+$content1 = "Welcome Guest<br />";
+$content2 = "<li class='nav-item'>
+		     <a class='nav-link' href='register.php'>Sign Up</a></li>
+			 <li class='nav-item'>
+		     <a class='nav-link' href='login.php'>Login</a></li>";
+
+if(isset($_SESSION["ShopperName"])) { 
+	//To Do 1 (Practical 2) - 
+    //Display a greeting message, Change Password and logout links 
+    //after shopper has logged in.
+	
+	//To Do 2 (Practical 4) - 
+    //Display number of item in cart
+	
+}
+?>
+<!-- To Do 3 (Practical 1) - 
+     Display a navbar which is visible before or after collapsing -->
+
+<!-- To Do 4 (Practical 1) - 
+     Define a collapsible navbar -->
+
