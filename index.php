0b8b66dab3824ab02993d170a58620176fec06a6
3thannn <ethan.leong@gmail.com>
2022-01-06 12:07:04 +0800

    added header and footer files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+// Detect the current session
+session_start();
+// Include the Page Layout header
+include("header.php"); 
+?>
+<img src="img/brandimage.png" class="img-fluid" 
+     style="display:block; margin:auto;"/>
+<?php 
+// Include the Page Layout footer
+include("footer.php"); 
+?>
