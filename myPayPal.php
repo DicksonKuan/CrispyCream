f6923253d2c6cd4b03fe741abae6a3a32a0c4536
srhysc <64086762+srhysc@users.noreply.github.com>
2022-01-11 03:48:31 +0800

    Update mypaypal.php

--- myPayPal.php
+++ myPayPal.php
@@ -5,9 +5,9 @@ $PayPalApiPassword 	= '9U5W96RKGKULGQAE'; 		//Paypal API password
 $PayPalApiSignature = 'APG-Ml.V-fbCSpsEVzwwLog0wn6jAP9zP.w6RwAy.MvvagxWO.y8q3pT'; 		//Paypal API Signature
 $PayPalCurrencyCode = 'SGD'; 	//Paypal Currency Code
 //URL to redirect to after PayPal has complete the online payment
-$PayPalReturnURL 	= 'http://localhost:8081/Mamaya/checkoutProcess.php'; 
+$PayPalReturnURL 	= 'http://localhost:8069/CrispyCream/checkoutProcess.php'; 
 //URL to redirect to if user clicks cancel                
-$PayPalCancelURL 	= 'http://localhost:8081/Mamaya/shoppingCart.php'; 
+$PayPalCancelURL 	= 'http://localhost:8069/CrispyCream/reviewOrder.php'; 
                 	
 function PPHttpPost($methodName_, $nvpStr_, $PayPalApiUsername, $PayPalApiPassword, 
                     $PayPalApiSignature, $PayPalMode) {
