8a6e142dc89164d4eb5172a8fc885f89f2b70a5c
Dickson Kuan <s10192803@connect.np.edu.sg>
2022-02-02 20:11:25 +0800

    Update category.php

--- category.php
+++ category.php
@@ -4,85 +4,68 @@ include("header.php"); // Include the Page Layout header
 ?>
 <!-- Create a container, 60% width of viewport -->
 <div style="width:100%; margin:auto;">
-<!-- Display Page Header -->
-<div class="row" style="padding:5px"> <!-- Start of header row -->
-    <div class="col-12">
-        <span class="page-title">Products category</span>
-        <p>Select a category listed below:</p>
-    </div>
-</div> <!-- End of header row -->
 <style>
-    
+     a{
+        color:black;
+    }
+    .card{
+        background-color:#f3e2cb;
+    }
+    a:hover{
+        color: #00754e;
+        text-decoration: none;
+    }
+    .btn{
+        background-color: #00754e;
+        color:white;
+    }
+    .btn:hover{
+        background-color: #ffe0b4;
+        color: black;
+    }
 </style>
+<div class="card-group">
 <?php 
 // Include the PHP file that establishes database connection handle: $conn
 include_once("mysql_conn.php");
 $qry = "SELECT * FROM Category ORDER BY CatName ASC";    // Form SQL to select all categories
 $result = $conn->query($qry);       //Execute the SQL and get the result
-
-//Card row 
-echo "<div class='row row-cols-1 row-cols-md-3 g-4'>";
-//Display each category in a row 
+$counter = 1;
 while($row = $result->fetch_array()){
-
-    //Card col
-    echo "<div class='col'>";
-    echo "<div class='card h-100'>";
-    
-    //Card header
     $img = "./img/category/$row[CatImage]";
-    echo "<h5 class='card-header'>
-            <img src='$img' class='w-25' alt='$row[CatName]'>
-        </h5>";
-
-    //Card body
-    echo "<div class='card-body'>";
-    echo "<h5>TOP SELLERS</h5>";
-    //Card carousel - to display top 3 offered item
-    $qry = "SELECT ProductImage FROM product 
+    $catname= urlencode($row["CatName"]);
+    $catproduct = "catProduct.php?cid=$row[CategoryID]&catName=$catname";
+    $qryProduct = "SELECT ProductImage FROM product 
             LEFT JOIN catproduct 
             ON product.ProductID = catproduct.ProductID 
             WHERE catproduct.CategoryID = $row[CategoryID]
             ORDER BY  product.offered DESC
-            LIMIT 3";   
-    $result2 = $conn->query($qry);       //Execute the SQL and get the result
-    echo "<div id='$row[CatName]' class='carousel slide' data-ride='carousel'>";
-    echo "<ol class='carousel-indicators'>";
-    echo "<li data-target='#$row[CatName]' data-slide-to='0' class='active'></li>";
-    echo "<li data-target='#$row[CatName]' data-slide-to='1'></li>";
-    echo "<li data-target='#$row[CatName]' data-slide-to='2'></li>";
-    echo "</ol>";
-    echo '<div class="carousel-inner">';
-    $counter = 1;
-    while($row2 = $result2->fetch_array()){
-        $img = "./img/Products/$row2[ProductImage]";
-        if($counter == 1){
-            echo "<div class='carousel-item active'> <img src='$img' class='d-block w-25' alt='...''></div>";
-        }else{
-            echo "<div class='carousel-item'> <img src='$img' class='d-block w-25' alt='...''></div>";
-        }
-        $counter +=1;
-    };
+            LIMIT 3";  
+    $resultProduct = $conn->query($qryProduct);
 
+    echo "<div class='card'>";
+    echo "<img src='$img' class='card-img-top mx-auto w-25 mt-5' alt='$row[CatName]'>";
+    echo "<div class='card-head mt-5'>";
     echo "</div>";
-    echo "</div>";    
+    echo "<div class='card-body'>";
+    echo "<div class='row'>";
+    //Display 3 donuts
+    while($rowProduct = $resultProduct->fetch_array()){
+        $productImg = "./img/products/$rowProduct[ProductImage]";
+        echo "<div class='col'>";
+        echo "<img src='$productImg' class='card-img-top mx-auto w-75'>";
+        echo "</div>";
+    }
+    echo "</div>";
+    echo "<a href='$catproduct'><h5 class='card-title text-center mt-5'>$row[CatName]</h5></a>";
+    echo "<p class='card-text text-center'>$row[CatDesc]</p>";
+    echo "</div></div>";
+    $counter += 1;
+}
 
-    echo "<p class='card-text'>$row[CatDesc]</p>";
-    echo "</div>";
-
-    //Card footer
-    $catname= urlencode($row["CatName"]);
-    $catproduct = "catProduct.php?cid=$row[CategoryID]&catName=$catname&page=1";
-    echo "<div class='card-footer'>";
-    echo "<a href=$catproduct class='btn btn-primary btn-sm'>View more</a>";
-    echo "</div>";
-
-    //End of card
-    echo "</div>";
-    echo "</div>";
-};  
-echo "</div>";
-
+?>
+</div>
+<?php 
 $conn->close(); // Close database connnection
 echo "</div>"; // End of container
 include("footer.php"); // Include the Page Layout footer
